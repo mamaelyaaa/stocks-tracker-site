903f495b3dff1a311c37a262995900c8a0d89060
mamaelyaaa <poopnp586@gmail.com>
2024-11-18 01:37:06 +0300

    feat: basic api, complete client session error

--- backend/src/http_client.py
+++ backend/src/http_client.py
@@ -10,12 +10,29 @@ class HTTPClient:
 
 class AVClient(HTTPClient):
 
-    async def get_trades_intraday(self):
-        self._params.update({
-            "function": "TIME_SERIES_INTRADAY",
-            "symbol": 'IBM',
-            "interval": '60min',
-        })
+    async def get_intraday_trades(self, symbol='IBM', interval='60min', **kwargs):
+        try:
+            self._params.update({
+                "function": "TIME_SERIES_INTRADAY",
+                "symbol": symbol,
+                "interval": interval,
+            })
+            self._params.update(**kwargs)
 
-        async with self._session.get(url='/query', params=self._params) as resp:
-            return await resp.json()
+            async with self._session.get(url='/query', params=self._params) as resp:
+                return await resp.json()
+        finally:
+            await self._session.close()
+
+    async def get_daily_trades(self, symbol='IBM', **kwargs):
+        try:
+            self._params.update({
+                "function": "TIME_SERIES_DAILY",
+                "symbol": symbol,
+            })
+            self._params.update(**kwargs)
+
+            async with self._session.get(url='/query', params=self._params) as resp:
+                return await resp.json()
+        finally:
+            await self._session.close()
--- backend/src/main.py
+++ backend/src/main.py
@@ -8,6 +8,11 @@ app = FastAPI()
 av_client = AVClient(base_url='https://www.alphavantage.co', api_key=settings.api_key)
 
 
-@app.get("/trades")
+@app.get("/trades/intraday")
 async def get_intraday_trades():
-    return await av_client.get_trades_intraday()
+    return await av_client.get_intraday_trades()
+
+
+@app.get("/trades/daily")
+async def get_daily_trades():
+    return await av_client.get_daily_trades()
